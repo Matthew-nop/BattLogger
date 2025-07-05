a18278d395f2b491203c9f1af7b0b536b9e42f83
Matthew C <Matthew.nop.dev@gmail.com>
2025-07-05 19:32:01 -0400

    Map routes for new pages

--- src/backend/pageRoutes.ts
+++ src/backend/pageRoutes.ts
@@ -4,13 +4,31 @@ import * as path from 'path';
 const distPath = path.join(__dirname, '..', '..', 'dist');
 
 export function setupPageRoutes(app: any) {
+	app.get('/model_details', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'model-details', 'model_details.html'));
+	});
 
-  app.get('/add_formfactor', (req: Request, res: Response) => {
-    res.sendFile(path.join(distPath, 'components', 'add-formfactor', 'add_formfactor.html'));
-  });
+	app.get('/battery_details/:batteryId', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'battery-details', 'battery_details.html'));
+	});
 
-  app.get('/add_chemistry', (req: Request, res: Response) => {
-    res.sendFile(path.join(distPath, 'components', 'add-chemistry', 'add_chemistry.html'));
-  });
+	app.get('/add_model', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'add-model', 'add_model.html'));
+	});
 
+	app.get('/add_formfactor', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'add-formfactor', 'add_formfactor.html'));
+	});
+
+	app.get('/add_chemistry', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'add-chemistry', 'add_chemistry.html'));
+	});
+
+	app.get('/add_battery', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'add-battery', 'add_battery.html'));
+	});
+
+	app.get('/', (req: Request, res: Response) => {
+		res.sendFile(path.join(distPath, 'components', 'main-page', 'index.html'));
+	});
 }
