144ced98d9c994a28ce1e6d1dc2c04ea19fe41cf
Matthew C <Matthew.nop.dev@gmail.com>
2025-07-12 22:09:53 -0400

    Fix broken jest test setup

--- tests/utils/testDbUtils.ts
+++ tests/utils/testDbUtils.ts
@@ -1,31 +1,23 @@
 import * as sqlite3 from 'sqlite3';
 
-import { loadModelDetails } from '../../src/backend/utils/dbUtils.js';
-import { stmtRunAsync } from '../../src/backend/utils/dbUtils.js';
 import { createTables } from '../../src/backend/utils/createTables.js';
-import { initializeDatabase } from '../../src/backend/utils/dbUtils.js';
+import { initializeDatabase, loadModelDetails, stmtRunAsync } from '../../src/backend/utils/dbUtils.js';
 
-import { ModelData } from '../../src/interfaces/interfaces.js';
 import { randomUUID } from 'crypto';
+import { ModelData } from '../../src/interfaces/interfaces.js';
 
 export const insertDummyValues = async (db: sqlite3.Database): Promise<void> => {
 	try {
-		const modelDetails: Map<string, ModelData> = loadModelDetails();
-		
+		const modelDetails: ModelData[] = loadModelDetails();
 
-		// Insert dummy battery and test data as part of application setup
 		const batteryStmt = db.prepare("INSERT INTO batteries (id, model_id) VALUES (?, ?)");
 		const testStmt = db.prepare("INSERT INTO battery_tests (battery_id, capacity, timestamp) VALUES (?, ?, ?)");
 
-		const modelKeys = Array.from(modelDetails.keys());
-		
-
 		for (let i = 0; i < 11; i++) {
-			const randomModelKey = modelKeys[Math.floor(Math.random() * modelKeys.length)];
-			const model: ModelData | undefined = modelDetails.get(randomModelKey);
-			if (model) {
+			const randomModel: ModelData = modelDetails[Math.floor(Math.random() * modelDetails.length)];
+			if (randomModel) {
 				const batteryId = randomUUID();
-				await stmtRunAsync(batteryStmt, [batteryId, model.id]);
+				await stmtRunAsync(batteryStmt, [batteryId, randomModel.id]);
 
 				// Insert 3 test records for each battery
 				for (let j = 0; j < 4; j++) {
